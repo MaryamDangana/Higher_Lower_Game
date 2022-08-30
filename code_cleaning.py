852190b817232f437fbabc74c308d3cbc3aaa753
Maryam Dangana <danganamaryam77@gmail.com>
2022-08-30 21:29:40 +0200

    code cleaning and scaling

--- code_cleaning.py
+++ code_cleaning.py
@@ -4,7 +4,7 @@ import random
 
 print(logo)
 score = 0
-x = Truecd Py
+x = True
 while x == True:
 
     compare_A = random.choice(data)
@@ -23,6 +23,7 @@ while x == True:
     statement_B = (
         f"{list(against_B.values())[0]}, a {list(against_B.values())[2]}, from {list(against_B.values())[3]}.")
 
+    ### part 2: Allow user to choose which key has highest value
 
     determinant = input(f"who has more followers: {statement_A} or {statement_B}?\n")
 
@@ -30,11 +31,25 @@ while x == True:
     B_final = 0
     max_values = max([A_key, B_key])
 
-    if max_values in list(compare_A.values()) and determinant == "A".lower():
-        score += 1
+    def find_key(input_dict):
+        for i in ([list(compare_A.values())[0] for k, v in input_dict.items() if v == A_key]):
+            global A_final
+            A_final = i
 
+    find_key(input_dict=compare_A)
+
+
+    def find_key2(input_dict):
+        for i in ([list(against_B.values())[0] for k, v in input_dict.items() if v == B_key]):
+            global B_final
+            B_final = i
+
+    find_key2(input_dict=against_B)
+
+    if max_values in list(compare_A.values()) and determinant == A_final:
+        score += 1
         print(f"Your score is {score}, you passed")
-    elif max_values in list(against_B.values()) and determinant == ("B".lower()):
+    elif max_values in list(against_B.values()) and determinant == B_final:
         score += 1
         print(f"Your score is {score}, you passed")
     else:
